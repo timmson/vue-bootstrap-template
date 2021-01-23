3e6d6a4c65efecc8d6462e7f982cc3751456593c
timmson <timmson666@mail.ru>
2021-01-24 00:36:33 +0300

    Migrate to Jest #7

--- test/test-calc.js
+++ test/test-calc.js
@@ -1,15 +1,13 @@
 const Calc = require("../src/calc");
-const {expect} = require("chai");
-require("mocha");
 
 describe("Calculate function", () => {
 
 	let calc = new Calc();
 
-	it("gets 'Ivan' and should return 398", () => {
+	test("gets 'Ivan' and should return 398", () => {
 		const result = calc.hash("Ivan");
 
-		expect(result).to.equal(398);
+		expect(result).toEqual(398);
 	});
 
 });
